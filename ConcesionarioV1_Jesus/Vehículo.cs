6967310a3ed4cd44f74f0ef425452dab6078bf4c
DAM 1 <DAM 1@DESKTOP-EN3MTPA>
2024-01-29 12:37:11 +0100

    Clase Vehículo: Miembros Privados, propiedades y constructores.

--- ConcesionarioV1_Jesus/Vehículo.cs
+++ ConcesionarioV1_Jesus/Vehículo.cs
@@ -11,14 +11,35 @@ namespace ConcesionarioV1_Jesus
         // CONSTANTES
 
         // MIEMBROS PRIVADOS
+        private string _marca;
+        private string _modelo;
 
         // CONSTRUCCIONES
+        public Vehículo(string marca, string modelo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+        }
 
         // PROPIEDADES
+        public string Marca
+        {
+            get { return _marca; }
+            set {
+                // TODO: Proporcionar seguridad y protección al miembro -> _marca
+                _marca = value; }
+        }
 
-        // MÉTODOS PÚBLICOS
+        public string Modelo
+        {
+            get { return _modelo; }
+            set
+            { // TODO: Proporcionar seguridad y protección al miembro -> _modelo
+              _modelo = value; }
+            }
+            // MÉTODOS PÚBLICOS
 
-        // MÉTODOS PRIVADOS
+            // MÉTODOS PRIVADOS
 
-    }
+        }
 }
