3e106622880ceeab4003eb26eb73407d2faa0106
DAM 1 <DAM 1@DESKTOP-EN3MTPA>
2024-01-29 12:04:22 +0100

    Agregar archivos de proyecto.

---
+++ ConcesionarioV1_Jesus/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConcesionarioV1_Jesus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // CONTROLADOR PRINCIPAL DE LA APLICACIÓN
+
+            // MENÚ DE ACCIONES
+
+            // PROCESO DE LAS ACCIONES
+
+        }
+    }
+}
